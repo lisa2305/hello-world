4f4b8c68d35de33bf35658f714b13c2240b9e92c
lisa2305 <lisa.giordani@marconirovereto.it>
2024-03-19 08:47:23 +0100

    First commit

---
+++ src/main/java/it/marconi/hello_world/HelloWorldApplication.java
@@ -0,0 +1,18 @@
+package it.marconi.hello_world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+}
+
+/*springboot è un framework 
+framework = ha una gestione tutta sua degli oggetti
+gitignore = file di git e contiene tutte le estensioni
+*/
