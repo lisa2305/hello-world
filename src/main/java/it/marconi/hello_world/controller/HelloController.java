5b1a9ad6edf1ae9b01a8a7cb86242d825e6e3e96
lisa2305 <lisa.giordani@marconirovereto.it>
2024-03-19 09:36:05 +0100

    controller commit

---
+++ src/main/java/it/marconi/hello_world/controller/HelloController.java
@@ -0,0 +1,12 @@
+package it.marconi.hello_world.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+//stereotype annotation
+@Controller //bin
+@RequestMapping("/")
+public class HelloController {
+
+}
