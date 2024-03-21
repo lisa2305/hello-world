570c4f85e5f055d7fde090ec5b461308960a916b
lisa2305 <lisa.giordani@marconirovereto.it>
2024-03-21 13:45:48 +0100

    basic get request + params

--- src/main/java/it/marconi/hello_world/controller/HelloController.java
+++ src/main/java/it/marconi/hello_world/controller/HelloController.java
@@ -2,11 +2,26 @@ package it.marconi.hello_world.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
-
-//stereotype annotation
+//stereotype annotation diventano un componente di springboot
+//invertion of control
 @Controller //bin
-@RequestMapping("/")
+@RequestMapping("/") //TUTTE
 public class HelloController {
+    //CRUD create read update delete -> http
 
+    @GetMapping //generica
+    public String greet(){
+        return "Hello-world "; //ha un riferimento ad una web page
+    }
+
+    //identificare un parametro
+    @GetMapping(params = "papero")
+    public String greetParameter() {
+        return "Hello-world-param";
+    }
+
+    //MVC
+    
 }
